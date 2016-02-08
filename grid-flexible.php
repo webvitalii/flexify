679dcbb285c5f7c89519654fa147ffd09c10c0b9
Vitalii <vitalii.skorokhod@gmail.com>
2016-02-08 14:59:14 -0600

    Added .fx-col and .fx-box with padding

--- grid-flexible.php
+++ grid-flexible.php
@@ -7,9 +7,9 @@
 	<p><a href="http://caniuse.com/#search=flexbox" target="_blank">Browsers support</a>: Modern browsers and IE12+</p>
 
 	<div class="fx-grid">
-		<div class="fx-box-1 fx-demo">.fx-box-1 - 25%<br> column with 2 lines</div>
-		<div class="fx-box-2 fx-demo">.fx-box-2 - 50%</div>
-		<div class="fx-box-1 fx-demo">.fx-box-1 - 25%</div>
+		<div class="fx-box fx-box-1 fx-demo">.fx-box-1 - 1/4 = 25%<br> .fx-box - adds inner padding</div>
+		<div class="fx-box fx-box-2 fx-demo">.fx-box-2 - 2/4 = 50%;</div>
+		<div class="fx-box fx-box-1 fx-demo">.fx-box-1 - 1/4 = 25%</div>
 	</div>
 
 
--- grid.php
+++ grid.php
@@ -5,9 +5,9 @@
 	<h1>Grid</h1>
 
 	<div class="fx-row">
-		<div class="fx-col-3 fx-demo">.fx-col-3<br> column with 2 lines</div>
-		<div class="fx-col-6 fx-demo">.fx-col-6</div>
-		<div class="fx-col-3 fx-demo">.fx-col-3</div>
+		<div class="fx-col fx-col-3 fx-demo">.fx-col-3 - 3/12 = 25%<br> .fx-col - adds inner padding</div>
+		<div class="fx-col fx-col-6 fx-demo">.fx-col-6 - 6/12 = 50%</div>
+		<div class="fx-col fx-col-3 fx-demo">.fx-col-3 - 3/12 = 25%</div>
 	</div>
 
 	<h2>Grid - responsive</h2>
@@ -15,9 +15,9 @@
 	<p><a href="http://caniuse.com/#feat=css-mediaqueries" target="_blank">Browsers support</a>: Modern browsers and IE9+</p>
 
 	<div class="fx-row">
-		<div class="fx-col-sm-3 fx-col-xs-2 fx-col-xl-5 fx-demo">.fx-col-sm-3 .fx-col-xs-2 .fx-col-xl-5<br> column with 2 lines</div>
-		<div class="fx-col-sm-6 fx-col-xs-2 fx-col-xl-5 fx-demo">.fx-col-sm-6 .fx-col-xs-2 .fx-col-xl-5</div>
-		<div class="fx-col-sm-3 fx-col-xs-8 fx-col-xl-2 fx-demo">.fx-col-sm-3 .fx-col-xs-8 .fx-col-xl-2</div>
+		<div class="fx-col fx-col-sm-3 fx-col-xs-2 fx-col-xl-5 fx-demo">.fx-col-sm-3 .fx-col-xs-2 .fx-col-xl-5<br> column with 2 lines</div>
+		<div class="fx-col fx-col-sm-6 fx-col-xs-2 fx-col-xl-5 fx-demo">.fx-col-sm-6 .fx-col-xs-2 .fx-col-xl-5</div>
+		<div class="fx-col fx-col-sm-3 fx-col-xs-8 fx-col-xl-2 fx-demo">.fx-col-sm-3 .fx-col-xs-8 .fx-col-xl-2</div>
 	</div>
 
 
