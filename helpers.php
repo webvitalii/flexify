bd2e0c478476a86d1799ca26e28f60141f83eb3a
Vitalii <vitalii.skorokhod@gmail.com>
2016-02-28 15:36:55 -0600

    Added helpers page

---
+++ helpers.php
@@ -0,0 +1,172 @@
+<?php include('inc/_header.php'); ?>
+
+<?php include('inc/_menu.php'); ?>
+
+	<h1>Helper classes</h1>
+	
+	<h2>Text helper classes</h2>
+	
+	<div class="fx-text-left fx-p-a fx-m-b fx-demo">.fx-text-left - aligning text to left</div>
+	
+	<div class="fx-text-center fx-p-a fx-m-b fx-demo">.fx-text-center - aligning text to center</div>
+	
+	<div class="fx-text-right fx-p-a fx-m-b fx-demo">.fx-text-right - aligning text to right</div>
+	
+	<div class="fx-text-lowercase fx-p-a fx-m-b fx-demo">.fx-text-lowercase - lowercase text</div>
+	
+	<div class="fx-text-uppercase fx-p-a fx-m-b fx-demo"><span class="fx-text-lowercase">.fx-text-uppercase</span> - uppercase text</div>
+	
+	<div class="fx-text-capitalize fx-p-a fx-m-b fx-demo"><span class="fx-text-lowercase">.fx-text-capitalize</span> - capitalize text</div>
+	
+	<div class="fx-text-mute fx-p-a fx-m-b fx-demo">.fx-text-mute - mute text</div>
+
+	<h2>Visibility helper classes</h2>
+
+<pre>	
+.fx-hide {
+	display: none !important;
+}
+
+.fx-show {
+	display: block !important;
+}
+
+.fx-invisible {
+	visibility: hidden;
+}
+</pre>
+
+	<h2>Margin helper classes</h2>
+
+<pre>	
+.fx-m-a-0 {
+	margin: 0 !important;
+}
+
+.fx-m-t-0 {
+	margin-top: 0 !important;
+}
+
+.fx-m-r-0 {
+	margin-right: 0 !important;
+}
+
+.fx-m-b-0 {
+	margin-bottom: 0 !important;
+}
+
+.fx-m-l-0 {
+	margin-left: 0 !important;
+}
+
+.fx-m-x-0 {
+	margin-right: 0 !important;
+	margin-left: 0 !important;
+}
+
+.fx-m-y-0 {
+	margin-top: 0 !important;
+	margin-bottom: 0 !important;
+}
+
+.fx-m-a {
+	margin: 15px !important;
+}
+
+.fx-m-t {
+	margin-top: 15px !important;
+}
+
+.fx-m-r {
+	margin-right: 15px !important;
+}
+
+.fx-m-b {
+	margin-bottom: 15px !important;
+}
+
+.fx-m-l {
+	margin-left: 15px !important;
+}
+
+.fx-m-x {
+	margin-right: 15px !important;
+	margin-left: 15px !important;
+}
+
+.fx-m-y {
+	margin-top: 15px !important;
+	margin-bottom: 15px !important;
+}
+
+.fx-m-x-auto {
+	margin-right: auto !important;
+	margin-left: auto !important;
+}
+</pre>
+
+	<h2>Padding helper classes</h2>
+
+<pre>	
+.fx-p-a-0 {
+	padding: 0 !important;
+}
+
+.fx-p-t-0 {
+	padding-top: 0 !important;
+}
+
+.fx-p-r-0 {
+	padding-right: 0 !important;
+}
+
+.fx-p-b-0 {
+	padding-bottom: 0 !important;
+}
+
+.fx-p-l-0 {
+	padding-left: 0 !important;
+}
+
+.fx-p-x-0 {
+	padding-right: 0 !important;
+	padding-left: 0 !important;
+}
+
+.fx-p-y-0 {
+	padding-top: 0 !important;
+	padding-bottom: 0 !important;
+}
+
+.fx-p-a {
+	padding: 15px !important;
+}
+
+.fx-p-t {
+	padding-top: 15px !important;
+}
+
+.fx-p-r {
+	padding-right: 15px !important;
+}
+
+.fx-p-b {
+	padding-bottom: 15px !important;
+}
+
+.fx-p-l {
+	padding-left: 15px !important;
+}
+
+.fx-p-x {
+	padding-right: 15px !important;
+	padding-left: 15px !important;
+}
+
+.fx-p-y {
+	padding-top: 15px !important;
+	padding-bottom: 15px !important;
+}
+</pre>
+	
+<?php include('inc/_footer.php'); ?>
