b7bfc2fedb514269b955d5905fda03a4a574411d
Vitalii <vitalii.skorokhod@gmail.com>
2016-04-07 17:00:26 -0500

    Added .fx-menu-light and .fx-menu-light-transparent classes

--- menu.php
+++ menu.php
@@ -4,6 +4,39 @@
 
 	<h1>Menu</h1>
 	
+	<h3>Menu light: .fx-menu .fx-menu-light</h3>
+	
+	<nav class="fx-menu fx-menu-light">
+		<ul>
+			<li><a href="index.php">Flexify</a></li>
+			<li class="page_item_has_children"><a href="grid.php">Docs</a>
+				<ul>
+					<li><a href="grid.php">Grid</a></li>
+					<li><a href="grid-flexible.php">Flexible Grid</a></li>
+				</ul>
+			</li>
+		</ul>
+	</nav>
+	
+	
+	<h3>Menu light transparent: .fx-menu .fx-menu-light-transparent</h3>
+	
+	<div style="float: left; width: 100%; background-color: #fff; padding: 50px;">
+	
+		<nav class="fx-menu fx-menu-light-transparent">
+			<ul>
+				<li><a href="index.php">Flexify</a></li>
+				<li class="page_item_has_children"><a href="grid.php">Docs</a>
+					<ul>
+						<li><a href="grid.php">Grid</a></li>
+						<li><a href="grid-flexible.php">Flexible Grid</a></li>
+					</ul>
+				</li>
+			</ul>
+		</nav>
+	
+	</div>
+	
 	<h3>Menu dark: .fx-menu .fx-menu-dark</h3>
 	
 	<nav class="fx-menu fx-menu-dark">
