868016bd4ad1e886bb5a54e272aa5e45613a7fe6
Vitalii <vitalii.skorokhod@gmail.com>
2016-04-24 22:27:29 -0500

    Reorganize menu

--- inc/_menu.php
+++ inc/_menu.php
@@ -3,23 +3,34 @@
 
 		<nav class="fx-menu fx-menu-dark">
 			<ul>
-				<li><a href="index.php">Flexify</a></li>
-				<li class="page_item_has_children"><a href="grid.php">Docs</a>
+				<li><a href="index.php">Flexify Home</a></li>
+				<li class="page_item_has_children"><a href="grid.php">Layout</a>
 					<ul>
 						<li><a href="grid.php">Grid</a></li>
 						<li><a href="grid-flexible.php">Flexible Grid</a></li>
 					</ul>
 				</li>
-				<li><a href="forms.php">Forms</a></li>
-				<li><a href="menu.php">Menu</a></li>
-				<li><a href="typography.php">Typography</a></li>
-				<li><a href="tables.php">Tables</a></li>
-				<li><a href="columns.php">Columns</a></li>
-				<li><a href="helpers.php">Helpers</a></li>
+				
+				<li class="page_item_has_children"><a href="typography.php">Content</a>
+					<ul>
+						<li><a href="typography.php">Typography</a></li>
+						<li><a href="tables.php">Tables</a></li>
+					</ul>
+				</li>
+				
+				<li class="page_item_has_children"><a href="menu.php">Components</a>
+					<ul>
+						<li><a href="menu.php">Menu</a></li>
+						<li><a href="forms.php">Forms</a></li>
+						<li><a href="columns.php">Columns</a></li>
+						<li><a href="helpers.php">Helpers</a></li>
+					</ul>
+				</li>
+				
 				<li class="page_item_has_children"><a href="http://flexify.webio.ca/" target="_blank">Links</a>
 					<ul>
-						<li><a href="http://flexify.webio.ca/" target="_blank">Flexify Website</a></li>
-						<li><a href="https://github.com/webvitalii/flexify" target="_blank">Flexify on GitHub</a></li>
+						<li><a href="//flexify.webio.ca/" target="_blank">Flexify Website</a></li>
+						<li><a href="//github.com/webvitalii/flexify" target="_blank">Flexify on GitHub</a></li>
 					</ul>
 				</li>
 			</ul>
