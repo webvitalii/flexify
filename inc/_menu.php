59f6e3605388d69c556afa6d43f94e55b7b2d0b8
Vitalii <vitalii.skorokhod@gmail.com>
2016-06-19 21:23:18 -0500

    Added labels

--- inc/_menu.php
+++ inc/_menu.php
@@ -31,6 +31,7 @@
 						<li><a href="menu.php">Menu</a></li>
 						<li><a href="forms.php">Forms</a></li>
 						<li><a href="columns.php">Columns</a></li>
+						<li><a href="labels.php">Labels</a></li>
 						<li><a href="helpers.php">Helpers</a></li>
 					</ul>
 				</li>
---
+++ labels.php
@@ -0,0 +1,22 @@
+<?php include('inc/_header.php'); ?>
+
+	<h1>Labels</h1>
+	
+	<ul>
+	
+		<li><span class="fx-label">Default - fx-label</span></li>
+		
+		<li><span class="fx-label fx-label-primary">Primary - fx-label fx-label-primary</span></li>
+		
+		<li><span class="fx-label fx-label-success">Success - fx-label fx-label-success</span></li>
+		
+		<li><span class="fx-label fx-label-info">Info - fx-label fx-label-info</span></li>
+		
+		<li><span class="fx-label fx-label-warning">Warning - fx-label fx-label-warning</span></li>
+		
+		<li><span class="fx-label fx-label-danger">Danger - fx-label fx-label-danger</span></li>
+	
+	</ul>
+
+	
+<?php include('inc/_footer.php'); ?>
