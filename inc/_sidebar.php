e40141fc94a9d43095421891af18bb215e1dbaad
Vitalii <vitalii.skorokhod@gmail.com>
2016-07-15 15:41:46 -0500

    Updated banners and ads in the sidebar

--- inc/_sidebar.php
+++ inc/_sidebar.php
@@ -1,23 +1,29 @@
 
 <div class="fx-box-sm-3 fx-padding-all fx-bg-gray-lightest">
 	
-	<div class="fx-text-center fx-margin-bottom">
-		<a href="//codecanyon.net/item/silver-bullet-pro/15171769?ref=webvitalii" target="_blank" title="Silver Bullet Pro - Speed up and Protect WordPress in a Smart Way" class="fx-btn fx-btn-primary">Speed Up WordPress</a>
+	<div class="fx-text-center">
+		<a href="https://codecanyon.net/item/silver-bullet-pro/15171769?ref=webvitalii" target="_blank" title="Speedup WordPress">
+			<img src="http://vitalii.webio.ca/wp-content/plugins/webio-kit/img/wordpress-speedup.png">
+			<div>Speedup WordPress</div>
+		</a>
 	</div>
-	
-	<div class="fx-text-center fx-margin-bottom">
-		<a href="//codecanyon.net/item/antispam-pro/6491169?ref=webvitalii" target="_blank" title="Anti-Spam Pro - Block Spam in WordPress Comments Section" class="fx-btn fx-btn-primary">Block WordPress Spam</a>
+		
+	<div class="fx-text-center">
+		<a href="https://codecanyon.net/item/antispam-pro/6491169?ref=webvitalii" target="_blank" title="Stop WordPress Spam">
+			<img src="http://vitalii.webio.ca/wp-content/plugins/webio-kit/img/wordpress-anti-spam.png">
+			<div>Stop WordPress Spam</div>
+		</a>
 	</div>
 	
 	<script async src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
-	<!-- flexify.webio.ca - responsive -->
+	<!-- Universal Responsive -->
 	<ins class="adsbygoogle"
 		 style="display:block"
 		 data-ad-client="ca-pub-5207574527239705"
-		 data-ad-slot="5124779271"
+		 data-ad-slot="3447834479"
 		 data-ad-format="auto"></ins>
 	<script>
 	(adsbygoogle = window.adsbygoogle || []).push({});
 	</script>
-
+	
 </div><!-- .fx-box -->
