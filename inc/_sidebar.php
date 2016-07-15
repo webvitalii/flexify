4ecbae4f83eacc933fa72e0f56c69a89f0517a66
Vitalii <vitalii.skorokhod@gmail.com>
2016-07-15 15:46:04 -0500

    Updated sidebar

--- inc/_sidebar.php
+++ inc/_sidebar.php
@@ -1,14 +1,14 @@
 
 <div class="fx-box-sm-3 fx-padding-all fx-bg-gray-lightest">
 	
-	<div class="fx-text-center">
+	<div class="fx-text-center fx-margin-bottom">
 		<a href="https://codecanyon.net/item/silver-bullet-pro/15171769?ref=webvitalii" target="_blank" title="Speedup WordPress">
 			<img src="http://vitalii.webio.ca/wp-content/plugins/webio-kit/img/wordpress-speedup.png">
 			<div>Speedup WordPress</div>
 		</a>
 	</div>
 		
-	<div class="fx-text-center">
+	<div class="fx-text-center fx-margin-bottom">
 		<a href="https://codecanyon.net/item/antispam-pro/6491169?ref=webvitalii" target="_blank" title="Stop WordPress Spam">
 			<img src="http://vitalii.webio.ca/wp-content/plugins/webio-kit/img/wordpress-anti-spam.png">
 			<div>Stop WordPress Spam</div>
